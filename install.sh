2757e7bd86ea851d8e1245d8abd8ae3773a7b02a
user <user@localhost.localdomain>
2020-09-25 17:38:53 -0400

    updates

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 echo "I'm not ready yet..."
 
-WHOAMI=user
+export WHOAMI=user
+
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root, exiting." 
@@ -23,6 +24,7 @@ yum install -y \
 
 singularity_install() {
 echo "Installing Go"
+if [ ! -d /usr/local/go ]; then
 export VERSION=1.13.1 OS=linux ARCH=amd64 && \
     wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
     sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
@@ -33,6 +35,7 @@ echo '
 echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
     echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
     source ~/.bashrc
+fi
 
 export VERSION=v3.6.3 && \
     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
@@ -45,9 +48,8 @@ export VERSION=v3.6.3 && \
 
 home_config() {
     if [ ! -d /home/$WHOAMI/.singularity ]; then
-	mkdir -p ~/.singularity
+	mkdir -p /home/$WHOAMI/.singularity
     fi
-
 
     if grep -q ghidra /home/$WHOAMI/.bashrc; then
 	echo "Aliases already found, skipping."
@@ -57,15 +59,14 @@ alias ghidra=\"singularity exec ~/.singularity/r2ghidra.sif ghidra\"
 alias r2=\"singularity exec ~/.singularity/r2ghidra.sif r2\"
 alias avrdude=\"singularity exec ~/.singularity/avrdude.sif avrdude\"
 alias qmk=\"singularity exec ~/.singularity/avrdude.sif qmk\"
-" >> ~/.bashrc
+" >> /home/$WHOAMI/.bashrc
     fi
-
 }
 export -f home_config
 
 container_building() {
     if [ -f ./buildfiles/avrdude.build ]; then
-	if [ -f /home/$WHOAMI/.singularity/avrdude.sif]; then
+	if [ -f /home/$WHOAMI/.singularity/avrdude.sif ]; then
 	    read -p "avrdude.sif exists, rebuild? (y/n):" avrconfirm
 	else
 	    singularity build /home/$WHOAMI/.singularity/avrdude.sif ./buildfiles/avrdude.build
@@ -77,7 +78,7 @@ container_building() {
 	fi
     fi
     if [ -f ./buildfiles/r2ghidra.build ]; then
-	if [ -f /home/$WHOAMI/.singularity/r2ghidra.sif]; then
+	if [ -f /home/$WHOAMI/.singularity/r2ghidra.sif ]; then
 	    read -p "r2ghidra.sif exists, rebuild? (y/n):" r2gconfirm
 	else
 	    singularity build /home/$WHOAMI/.singularity/r2ghidra.sif ./buildfiles/r2ghidra.build
@@ -90,6 +91,7 @@ container_building() {
     fi
 }
 
-
-su -m $WHOAMI -c "bash -c home_config $USERDIR"
+su -m $WHOAMI -c "bash -c home_config"
+singularity_install
+container_building
 
